4f2f2cb31265ee9c384754b5fc1ba33e49d66aeb
owtotwo <owtotwo@163.com>
2016-10-15 11:39:32 +0800

    js done

--- static/index.js
+++ static/index.js
@@ -1,11 +1,11 @@
 function Done(user, id) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://sysu.top/"+ user+ "/" + id, false);
+    xmlHttp.open( "GET", "/"+ user+ "/" + id, false);
     location.reload(true);
 }
 
 function Undo(user, id) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://sysu.top/"+ user+ "/" + id, false);
+    xmlHttp.open( "GET", "/"+ user+ "/" + id, false);
     location.reload(true);
 }
